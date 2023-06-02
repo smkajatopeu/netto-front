705afaa6f74abd15cfb95e0b7c62523ee3e71548
smkajatopeu <smkajatopeu@gmail.com>
2023-06-02 19:48:24 +0600

    feat/fbs-data-clean

--- firebase.ts
+++ firebase.ts
@@ -7,13 +7,13 @@ import 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyB0GBUW7UinTt6RRfmv-SI5dGZiFAchBQc',
-	authDomain: 'netto-4cff2.firebaseapp.com',
-	projectId: 'netto-4cff2',
-	storageBucket: 'netto-4cff2.appspot.com',
-	messagingSenderId: '883831304702',
-	appId: '1:883831304702:web:ddf713bc81133d877510af',
-	measurementId: 'G-LW26X242V6',
+	apiKey: '################################',
+	authDomain: '###########################',
+	projectId: '############',
+	storageBucket: '###################',
+	messagingSenderId: '###########',
+	appId: '################################',
+	measurementId: '##########',
 }
 
 // Initialize Firebase
